9c088d9d3075903386923ed5ac66c2e252b94f58
igwan <igwano@gmail.com>
2016-10-15 12:06:35 +0200

    add a basic InputController.

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,22 +1,53 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
-using System.Collections;
+using System;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(PlayerController))]
 public class InputController : MonoBehaviour
 {
-	MusicTempo mTempo ;
+    PlayerController playerController;
+	MusicTempo mTempo;
 
-	void Start(){
-		mTempo = new MusicTempo ();
+    Dictionary<string, Action> actions;
+
+	void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+        actions = new Dictionary<string, Action>()
+        {
+            { "Jump", playerController.Jump },
+            { "Stop", playerController.Stop },
+            { "Walk", playerController.Walk },
+            { "Sprint", playerController.Sprint }
+        };
+
+        mTempo = SoundManager.Instance.musicTempo;
 	}
-	
+
+    void ProcessActions()
+    {
+        foreach(string inputName in actions.Keys)
+        {
+            if(Input.GetButtonDown(inputName))
+            {
+                if(!mTempo.isInToleranceNow())
+                    Debug.Log("missed");
+                else
+                    actions[inputName]();
+            }
+        }
+    }
+
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		if (Input.GetKeyDown ("space") && mTempo.isInToleranceNow ()) {
 			Debug.Log ("gg");
 		} else if(Input.GetKeyDown ("space")){
 			Debug.Log ("noob");
 		}
+
+        ProcessActions();
 	}
 }
