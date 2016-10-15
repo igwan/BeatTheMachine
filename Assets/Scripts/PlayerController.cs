19557505cc5b39baab49d4092549f61c598e98d6
igwan <igwano@gmail.com>
2016-10-15 12:06:20 +0200

    add a dummy PlayerController.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public void Jump()
+    {
+        Debug.Log("Jump");
+    }
+
+    public void Sprint()
+    {
+        Debug.Log("Sprint");
+    }
+
+    public void Stop()
+    {
+        Debug.Log("Stop");
+    }
+
+    public void Walk()
+    {
+        Debug.Log("Walk");
+    }
+}
