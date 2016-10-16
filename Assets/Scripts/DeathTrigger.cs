ec162758e4b6b35b84f6fd8f45479933e639fe34
igwan <igwano@gmail.com>
2016-10-16 05:20:38 +0200

    add a DeathTrigger.

---
+++ Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTrigger : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            var player = other.gameObject.GetComponent<PlayerController>();
+            //player.Death();
+        }
+    }
+}
